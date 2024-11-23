7c17fe1ccbb08f01b15b8264a961324482d3400f
Dmitrii Zakharov <dmitrza1991@yandex.ru>
2024-11-23 13:42:13 +0100

    add paratemerization to classes BunTest, IngredientTest

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,23 +1,45 @@
 package praktikum;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.hamcrest.CoreMatchers.is;
+import static praktikum.IngredientType.FILLING;
+import static praktikum.IngredientType.SAUCE;
 
+@RunWith(Parameterized.class)
 public class BunTest {
+
+    public static Database database = new Database();
+
+    @Parameterized.Parameter
+    public String bunName;
+
+    @Parameterized.Parameter(1)
+    public float bunPrice;
+
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        return new Object[][]{
+                {database.availableBuns().get(0).getName(), database.availableBuns().get(0).getPrice()},
+                {database.availableBuns().get(1).getName(), database.availableBuns().get(1).getPrice()},
+                {database.availableBuns().get(2).getName(), database.availableBuns().get(2).getPrice()}
+        };
+    }
 
     @Test
     public void getName() {
-        Bun bun = new Bun("black bun", 100.0F);
-        assertEquals("black bun", bun.getName());
+        Bun bun = new Bun(bunName, bunPrice);
+        assertEquals(bunName, bun.getName());
     }
 
     @Test
     public void getPrice() {
-        Bun bun = new Bun("black bun", 100.0F);
-        assertThat(bun.getPrice(), is(100.0F));
+        Bun bun = new Bun(bunName, bunPrice);
+        assertThat(bun.getPrice(), is(bunPrice));
     }
 
 }
--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -1,27 +1,53 @@
 package praktikum;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class IngredientTest {
+
+    public static Database database = new Database();
+
+    @Parameterized.Parameter
+    public IngredientType ingredientType;
+
+    @Parameterized.Parameter(1)
+    public String ingredientName;
+
+    @Parameterized.Parameter(2)
+    public float ingredientPrice;
+
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        return new Object[][]{
+                {database.availableIngredients().get(0).getType(), database.availableIngredients().get(0).getName(), database.availableIngredients().get(0).getPrice()},
+                {database.availableIngredients().get(1).getType(), database.availableIngredients().get(1).getName(), database.availableIngredients().get(1).getPrice()},
+                {database.availableIngredients().get(2).getType(), database.availableIngredients().get(2).getName(), database.availableIngredients().get(2).getPrice()},
+                {database.availableIngredients().get(3).getType(), database.availableIngredients().get(3).getName(), database.availableIngredients().get(3).getPrice()},
+                {database.availableIngredients().get(4).getType(), database.availableIngredients().get(4).getName(), database.availableIngredients().get(4).getPrice()},
+                {database.availableIngredients().get(5).getType(), database.availableIngredients().get(5).getName(), database.availableIngredients().get(5).getPrice()}
+        };
+    }
 
     @Test
     public void getPrice() {
-        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "sausage", 200.0F);
-        assertEquals(200.0F, ingredient.getPrice(), 0);
+        Ingredient ingredient = new Ingredient(ingredientType, ingredientName, ingredientPrice);
+        assertEquals(ingredientPrice, ingredient.getPrice(), 0);
     }
 
     @Test
     public void getName() {
-        Ingredient ingredient = new Ingredient(IngredientType.FILLING, "sausage", 150.0F);
-        assertEquals("sausage", ingredient.getName());
+        Ingredient ingredient = new Ingredient(ingredientType, ingredientName, ingredientPrice);
+        assertEquals(ingredientName, ingredient.getName());
     }
 
     @Test
     public void getType() {
-        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "sausage", 5345.0F);
-        assertEquals(IngredientType.SAUCE, ingredient.getType());
+        Ingredient ingredient = new Ingredient(ingredientType, ingredientName, ingredientPrice);
+        assertEquals(ingredientType, ingredient.getType());
     }
 
 }
